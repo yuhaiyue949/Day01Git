99632a1148ffebbb43228cb550b5746bf6675d03
yuhaiyue949 <1982763570@qq.com>
2017-09-11 16:29:22 +0800

    第二次提交

---
+++ app/src/main/java/com/example/day01git/Bean.java
@@ -0,0 +1,14 @@
+package com.example.day01git;
+
+import android.util.Log;
+
+/**
+ * Created by 于海月 on 2017/9/11.
+ */
+
+public class Bean {
+    public  void  Data(){
+        Log.e("TAG", "Data:吴佩印是猪八戒，哈哈哈哈 " );
+
+    }
+}
