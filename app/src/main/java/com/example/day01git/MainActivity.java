9433d5f399a2d3e1672ce0ea8503203d36d03d5b
yuhaiyue949 <1982763570@qq.com>
2017-09-11 16:34:17 +0800

    第4次提交

--- app/src/main/java/com/example/day01git/MainActivity.java
+++ app/src/main/java/com/example/day01git/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("TAG", "onCreate: 第一次修改" );
+        Log.e("TAG", "onCreate: 第3次修改" );
     }
 }
